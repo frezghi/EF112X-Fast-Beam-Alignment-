1d319d5c5bbc6de6f0f1b303d67c91bccbdedeb7
frezghi <frezghi@github.com>
2024-04-17 00:02:19 +0200

    fixed bug in how data was organized which caused scrambled classes

--- xgboost_model.py
+++ xgboost_model.py
@@ -16,19 +16,23 @@ data_set_label_transmission = np.load('data/data_label_transmission.npy')
 # Reshape input data for XGBoost
 X = data_set_input.reshape(len(data_set_input), -1)
 
-# y = []
 
-# for label in data_set_label:
-#     row = np.arange(num_antenna_bs)
-#     row[label] = 1
-#     y.append(row)
+# Adds dummy data for missing classes since XGB requires each class to appear atleast once in training set
+# Adds the data twice since it will be split into train/test with stratify enabled
+for i in range(num_antenna_bs):
+    if i not in data_set_label:
+        data_set_label = np.append(data_set_label, [i,i])
+        similar_index = np.argwhere(data_set_label == i-1)[0]
+        X = np.append(X, X[similar_index], axis=0)
+        X = np.append(X, X[similar_index], axis=0)
+        data_set_label_transmission = np.append(data_set_label_transmission, data_set_label_transmission[similar_index], axis=0)
+        data_set_label_transmission = np.append(data_set_label_transmission, data_set_label_transmission[similar_index], axis=0)
 
-# y = np.array(y)
+#print(np.shape(X), np.shape(data_set_label), np.shape(data_set_label_transmission))
 
-le = LabelEncoder()
-y = le.fit_transform(data_set_label)
+y = data_set_label
 
-X_train, X_test, y_train, y_test, transmission_train, transmission_test = train_test_split(X, y, data_set_label_transmission, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test, transmission_train, transmission_test = train_test_split(X, y, data_set_label_transmission, test_size=0.2, random_state=42, stratify=y)
 
 # Define the XGBoost model
 print("Fit model")
@@ -41,8 +45,19 @@ y_pred = model.predict(X_test)
 curr_sum = 0
 num_elements = 0 
 for predicited_channel, best_channel, channel_transmission in zip(y_pred, y_test, transmission_test):
-    print(channel_transmission[predicited_channel], max(channel_transmission))
-    curr_sum += channel_transmission[predicited_channel]/max(channel_transmission)
+    # Search neighbouring channels, but make sure to stay in bounds of array
+    try:
+        final_prediction = max(channel_transmission[predicited_channel-1], channel_transmission[predicited_channel], channel_transmission[predicited_channel+1])
+    except IndexError:
+        try:
+            final_prediction = max(channel_transmission[predicited_channel-1], channel_transmission[predicited_channel], channel_transmission[predicited_channel-2])
+        except IndexError:
+            final_prediction = max(channel_transmission[predicited_channel+1], channel_transmission[predicited_channel], channel_transmission[predicited_channel+2])
+
+    print(channel_transmission)
+    print(final_prediction, channel_transmission[best_channel])
+
+    curr_sum += final_prediction/channel_transmission[best_channel]
     num_elements += 1
 
 print(f'Average percent of power from predicted channel compared to best channel {100*curr_sum/num_elements}%')
